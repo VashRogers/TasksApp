1eb2484b82db9cf5787dc81c99b55424547af46c
VashRogers <rogerssj2@gmail.com>
2022-06-07 22:02:39 -0400

    Alteraçoes pequenas

--- src/screens/Auth.js
+++ src/screens/Auth.js
@@ -49,7 +49,7 @@ export default class Auth extends Component {
           password: this.state.password
         });
         axios.defaults.headers.common['Authorization'] = `bearer ${res.data.token}`
-        this.props.navigation.navigate('Home');
+        this.props.navigation.navigate('Home', res.data);
       }
       catch(e){
         showError(e)
